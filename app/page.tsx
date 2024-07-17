4eb8aef2252bc0023068c02f5e945b1522aae414
Jesus Vera <yue.vera@gmail.com>
2024-07-17 11:01:15 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="font-sans h-40 bg-pink text-gray-dark-2">
+        <div className="text-5xl">character designer</div>
+      </header>
+      <main className="h-full">a</main>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,49 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      blue: '#1fb6ff',
+      purple: '#6221e6',
+      pink: '#fedadb',
+      'gray-dark': '#273444',
+      'gray-dark-2': '#302939',
+      gray: '#333333',
+      'gray-light': '#c7c6c6',
+      white: '#ffffff',
+      black: '#1e1e1e',
+    },
+    fontFamily: {
+      sans: ['Jost', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [require('preline/plugin')],
+}
+export default config
